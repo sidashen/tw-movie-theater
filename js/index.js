cb4cc87ece80cf74b071b229d14d7339d8e224e1
sidashen <shashasi8023@163.com>
2020-01-12 22:24:35 +0800

    [shasha] feat: classify movie function

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-getResource();
+let movieData;
 
-function getResource() {
+const getResource = () => {
   myAjax(
     'https://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a', 
     'get', 
     {}, 
     function (res) {
       loadAllMovie(res);
-      loadLoveMovie(res);
+      movieData = res;
     }
   );
 }
@@ -31,15 +31,9 @@ const loadAllMovie = (res) => {
         <p class="card-text">类别: ${item.genres}</p>
         <a href=${item.alt} target="_blank" class="btn btn-primary">查看详情</a>
         </div>
-        </div>`
+      </div>`
       });
     $('.movie-show-lists').html(list);
-    $('.movie-show-list div').each(function(index, element) {
-      var idx=index+1;
-      if (!(idx%5)){
-        $(this).after('<br/>');
-  }
-  });
 }
 
 const handleSearch = () => {
@@ -90,53 +84,60 @@ const searchMovie = (res) => {
   $('input')[0].value = '';
 }
 
-const loadLoveMovie = (res) => {
-  const loveMovieLists = res.subjects.filter(item => {
-    return item.genres.includes('剧情');
+const loadMovieClass = (event) => {
+  const movieLists = movieData.subjects.filter(item => {
+    if (event.target.className === 'story') {
+      return item.genres.includes('剧情');
+    } else if (event.target.className === 'action') {
+      return item.genres.includes('动作');
+    }
   });
-  if (loveMovieLists.length) {
+
+  if (movieLists.length) {
     let list = '';
-    loveMovieLists.forEach(item => {
+    movieLists.forEach(item => {
       list += `<div class="movie-card">
-      <img class="card-img-top" src=${item.images.medium} alt="Card image cap">
-      <div class="card-body">
-      <h5 class="card-title">${item.title}</h5>
-      <p class="card-text">年份: ${item.year}</p>
-      <p class="card-text">评分: ${item.rating.average}</p>
-      <p class="card-text">导演: ${item.casts.map(
-        item => item.name
-      )}</p>
-      <p class="card-text">演员: ${item.casts.map(
-        item => item.name
-      )}</p>
-      <p class="card-text">类别: ${item.genres}</p>
-      <a href=${item.alt} target="_blank" class="btn btn-primary">查看详情</a>
-      </div>
+        <img class="card-img-top" src=${item.images.medium} alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">${item.title}</h5>
+        <p class="card-text">年份: ${item.year}</p>
+        <p class="card-text">评分: ${item.rating.average}</p>
+        <p class="card-text">导演: ${item.casts.map(
+          item => item.name
+        )}</p>
+        <p class="card-text">演员: ${item.casts.map(
+          item => item.name
+        )}</p>
+        <p class="card-text">类别: ${item.genres}</p>
+        <a href=${item.alt} target="_blank" class="btn btn-primary">查看详情</a>
+        </div>
       </div>`
     });
+    $('.movie-show-lists').html(list);
   }
 }
+
+getResource();
 
 $('body').click(event => {
   let {classList} = event.target;
 
   if (classList.contains('btn')) {
-    handleSearch();
+    handleSearch(res);
   }
   if (classList.contains('story')) {
-    loadLoveMovie();
+    loadMovieClass(event);
+  }
+  if (classList.contains('action')) {
+    loadMovieClass(event);
+  }
+  if (classList.contains('love')) {
+    loadMovieClass(event);
   }
 });
 
-$('input')[0].keydown((e) => {
+$('.form-control').keydown((e) => {
   if (e.which === 13) {
     handleSearch();
   }
 });
-
-
-
-
-
-
-
