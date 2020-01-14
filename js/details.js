bea69c1d47f1a8cbef27b7908ed99e694f47a43f
sidashen <shashasi8023@163.com>
2020-01-14 10:16:15 +0800

    [shasha]: feat: movie summary

--- js/details.js
+++ js/details.js
@@ -13,24 +13,25 @@ const getSpecificResource = () => {
 }
 
 const loadSpecificMovie = (res) => {
-  let list = '';
-  list = `<h2 class="card-title">${res.title} (${res.year})</h2>
+  const list = `<h2 class="card-title">${res.title} (${res.year})</h2>
     <img class="card-img-top" src=${res.images.small} alt="Card image cap">
     <div class="card-body">
     <p class="card-text">导演: ${res.directors.map(
       item => item.name
-    )}</p>
+  )}</p>
     <p class="card-text">演员: ${res.casts.map(
       item => item.name
-    )}</p>
+  )}</p>
     <p class="card-text">类别: ${res.genres}</p>
     <p class="card-text">制片国家/地区: ${res.countries}</p>
     <p class="card-text">年份: ${res.year}</p>
     <p class="card-text">又名: ${res.aka}</p>
     <p class="card-text">评分: ${res.rating.average}</p>
     <a href="${res.alt}" target="_blank"><button class="movie-description">在线观看</button></a>
-    </div>`
+    </div>
+    <h4>剧情简介</h4>
+    <p class="card-text">${res.summary}</p>`;
   $('.movie-details').html(list);
-}
+};
 
 getSpecificResource();
